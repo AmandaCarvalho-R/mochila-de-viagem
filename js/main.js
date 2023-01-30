2b788cd1e886048e476802a2a4f6cd4e98e97bc7
Amanda Carvalho <amanda.chrys370@hotmail.com>
2023-01-29 23:16:11 -0400

    Criando novo elemento na lista em JS

--- js/main.js
+++ js/main.js
@@ -3,9 +3,24 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-
-console.log(evento)
-console.log(evento.target.elements['nome'].value)
-console.log(evento.target.elements['quantidade'].value)
+criarElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 
 })
+
+function criarElemento(nome, quantidade){
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem)
+}
